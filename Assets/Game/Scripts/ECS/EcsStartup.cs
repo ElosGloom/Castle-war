ce37024dcc3da743884572a7733e7a0e57033eb1
tka2704 <tka2704@gmail.com>
2024-10-14 23:17:49 +0300

    startup refactor

--- Assets/Game/Scripts/ECS/EcsStartup.cs
+++ Assets/Game/Scripts/ECS/EcsStartup.cs
@@ -1,3 +1,4 @@
+using Common;
 using ECS.Systems;
 using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
@@ -7,37 +8,42 @@ namespace ECS
 {
     public class EcsStartup : MonoBehaviour
     {
-        EcsSystems _systems;
-
-
-        void Update()
-        {
-            _systems?.Run();
-        }
-
-        void OnDestroy()
-        {
-            _systems?.Destroy();
-            _systems?.GetWorld()?.Destroy();
-            _systems = null;
-        }
+        private EcsSystems _systems;
 
         public void Init()
         {
             var world = new EcsWorld();
             _systems = new EcsSystems(world);
-
             _systems
+#region Debug
 #if UNITY_EDITOR
                 .Add(new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem())
                 .Add(new Leopotam.EcsLite.UnityEditor.EcsSystemsDebugSystem())
 #endif
+#endregion
+#region UI
+#endregion
+#region PreBattle
                 .Add(new UnitSpawnSystem())
-                
-                .Inject()
+#endregion
+#region Battle
+#endregion
+                .Inject(new RuntimeTempData())
                 .Init();
 
             enabled = true;
+        }
+
+        private void Update()
+        {
+            _systems?.Run();
+        }
+
+        private void OnDestroy()
+        {
+            _systems?.Destroy();
+            _systems?.GetWorld()?.Destroy();
+            _systems = null;
         }
     }
 }
